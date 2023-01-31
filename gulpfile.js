cee0cca9da07b95dae941603b546a5e3bc37ec9a
KevinJSS <kevin.alvarado@ucrso.info>
2023-01-31 12:09:22 -0600

    Webp & Avif image conversion process added

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ const autoprefixer = require('autoprefixer');
 
 // Image Processing
 const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function compileCSS( done ) {
 
@@ -27,23 +29,54 @@ function compileCSS( done ) {
 }
 
 function processImages( done ) {
+
     //Image processing task
+    //1. Identify the source files
+    //2. Optimize the images
+    //3. Save the files
+
     src('src/images/**/*')
         .pipe( imagemin({ optimizationLevel: 3 }) )
         .pipe( dest('build/images') );
     done();
 } 
 
+function webpConversion( ) {
+
+    //Webp conversion task
+    //1. Identify the source files (jpg & png) 
+    //2. Convert the images to webp
+    //3. Save the files
+
+    return src('src/images/**/*.{png,jpg}')
+        .pipe( webp({ quality: 50 }) )
+        .pipe( dest('build/images') );
+}
+
+function avifConversion() {
+    
+        //Avif conversion task
+        //1. Identify the source files (jpg & png) 
+        //2. Convert the images to avif
+        //3. Save the files
+    
+        return src('src/images/**/*.{png,jpg}')
+            .pipe( avif({ quality: 50 }) )
+            .pipe( dest('build/images') );
+}
+
 function watchFileChanges() {
-    //Watch for file changes
+    //Watch for files changes
     watch( 'src/scss/**/*.scss', compileCSS ); //<-- all the .scss files in the folder
     watch( 'src/images/**/*', processImages );
 }
 
 exports.compileCSS = compileCSS;
 exports.processImages = processImages;
+exports.webpConversion = webpConversion;
+exports.avifConversion = avifConversion;
 exports.watchFileChanges = watchFileChanges;
-exports.default = series( processImages, compileCSS, watchFileChanges );
+exports.default = series( processImages, webpConversion, avifConversion, compileCSS, watchFileChanges );
 
 // Default taks --> Just run gulp in the terminal to the tasks you've specified
 // Series --> Run the tasks in order
